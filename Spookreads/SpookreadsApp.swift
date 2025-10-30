51547c8055e70d6474661a84b5a9da97d5a18e96
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-30 00:24:51 +0000

    Initial Commit

---
+++ Spookreads/SpookreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpookreadsApp.swift
+//  Spookreads
+//
+//  Created by Evan Plant on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpookreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
